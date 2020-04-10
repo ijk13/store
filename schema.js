90c32b94130334f2bac5ed25c6b03f6136a28f38
ijk13 <40337918+ijk13@users.noreply.github.com>
2020-04-10 09:00:27 +0530

    Update schema.js

--- schema.js
+++ schema.js
@@ -1,6 +1,6 @@
   
 let mongoose  = require("./config");
 mongoose = mongoose.mongoose
-const nstoreSchema = new mongoose.Schema({brand: String, product:String,price:String,unit:String});
+const nstoreSchema = new mongoose.Schema({brand: String, product:String,price:String,unit:String,img:String});
 const nstore = mongoose.model('store', nstoreSchema);
 module.exports.nstore = nstore;
